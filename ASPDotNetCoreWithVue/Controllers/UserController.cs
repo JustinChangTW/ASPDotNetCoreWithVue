867e53944599d7b6c8458e92013e9bf5f9f2b232
justin <justin@arcran.com>
2020-09-04 14:26:36 +0800

    修改由後端驗證登入

---
+++ ASPDotNetCoreWithVue/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPDotNetCoreWithVue.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDotNetCoreWithVue.Controllers
+{
+    public class UserController : Controller
+    {
+        public JsonResult Login([FromBody] UserAuthInfo user)
+        {
+            return new JsonResult(new { 
+                code = 20000, 
+                data = new { token = "abcd" } });
+        }
+
+        public JsonResult Info(string token)
+        {
+            return new JsonResult(new {
+                code = 20000, 
+                data = GetUserInfo("abcd") });
+        }
+        public JsonResult Logout()
+        {
+            return new JsonResult(new {
+                code = 20000, 
+                data= "success" });
+        }
+
+        public UserInfo GetUserInfo(string Token)
+        {
+            return new UserInfo { 
+                Id = "1", 
+                Name = "Super Admin",
+                Introduction = "I am a super administrator",
+                Avatar = "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
+                roles = new List<string> { "Admin" }
+            };
+        }
+    }
+}
---
+++ ASPDotNetCoreWithVue/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPDotNetCoreWithVue.Models
+{
+    public class UserInfo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Introduction { get; set; }
+        public string Avatar { get; set; }
+        public string Token { get; set; }
+        public List<string> roles { get; set; }
+    }
+
+    public class UserAuthInfo
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
